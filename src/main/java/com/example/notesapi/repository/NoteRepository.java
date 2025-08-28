bb5d2bfe7b0ac0fdc76073311c3a993837d4777f
Juan Moreira <juanmoreiracosta2016@gmail.com>
2025-08-28 19:48:55 -0300

    Creación de la interfaz NoteRepository

---
+++ src/main/java/com/example/notesapi/repository/NoteRepository.java
@@ -0,0 +1,7 @@
+package com.example.notesapi.repository;
+
+import com.example.notesapi.entity.Note;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+}
